a7d4d95fda0440fb919364eb2948c2b8ad15e337
skycloud55me-bit <skycloud55me@gmail.com>
2025-10-22 22:46:38 +0100

    Create voice-assistant.js

---
+++ voice-assistant.js
@@ -0,0 +1,16 @@
+// إدارة الصوت (نسخة مبسطة)
+class VoiceAssistant {
+    constructor() {
+        this.isListening = false;
+    }
+
+    // سيتتم تطوير هذه الوظائف في المراحل القادمة
+    startListening() {
+        console.log('بدء الاستماع...');
+        // هنا سيتم إضافة Web Speech API
+    }
+
+    stopListening() {
+        console.log('توقف الاستماع');
+    }
+}
